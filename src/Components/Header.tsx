6389f64251dac102e8b51bdf9c2e647c27795055
minjuwon <suju412@naver.com>
2025-12-29 10:51:09 +0900

    header 디자인 완료

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,4 +1,85 @@
+import {twMerge} from "tailwind-merge";
+import {Link} from "react-router";
+import {MdCampaign, MdDarkMode, MdMenu, MdSearch, MdVideoCall} from "react-icons/md";
+import {FaRegUserCircle, FaYoutube} from "react-icons/fa";
+
 function Header() {
-    return<></>
+    return<header className={twMerge(
+        ['fixed','top-0','left-0','right-0','h-header','px-4'],
+        ['border-b','border-divider'],
+        ['flex','justify-between','items-center'],
+        ['z-50']
+    )}>
+        {/*왼족*/}
+        <div className={twMerge(['flex','items-center','gap-4'])}>
+            <button className={twMerge(
+                ['p-2','rounded-full','hover:bg-text-default/10']
+            )}>
+                <MdMenu className={twMerge(['w-6','h-6'])}/>
+            </button>
+            <Link to={"/"} className={'flex items-center gap-2'}>
+                <FaYoutube className={twMerge(['w-8','h-8','text-primary-main'])}/>
+                <span className={'text-xl font-bold italic'}>SolTube</span>
+            </Link>
+        </div>
+        {/*가운데*/}
+        <div className={twMerge(
+            ['flex-1','max-w-[600px]'],
+            ['flex','items-center']
+        )}>
+            <div className={'flex w-full'}>
+                <input
+                    placeholder={"검색"}
+                    className={twMerge(
+                        ['border','border-divider','shadow-inner'],
+                        ['w-full','rounded-l-full','px-4','py-2'],
+                        ['text-text-default','placeholder:text-text-disabled'],
+                        ['focus:outline-none','focus:border-secondary-main'],
+                        )}
+                />
+                <button className={twMerge(
+                    ['px-4','py-2'],
+                    ['border','border-divider','rounded-r-full','border-l-0']
+                )}>
+                    <MdSearch className={twMerge(['w-6','h-6'])}/>
+                </button>
+            </div>
+        </div>
+        {/*오른쪽*/}
+        <div className={twMerge(['flex','items-center','gap-2'])}>
+            <Link to={"/notices"} className={twMerge(
+                ['flex','items-center','justify-center','p-2'],
+                ['rounded-full','hover:bg-text-default/10']
+            )}
+                  title={"공지사항"}
+            >
+                <MdCampaign className={'w-6 h-6'}/>
+            </Link>
+            <button className={twMerge(
+                ['flex','items-center','justify-center','p-2'],
+                ['rounded-full','hover:bg-text-default/10']
+            )}
+                    title={"다크모드로 변경"}
+            >
+                <MdDarkMode className={'w-6 h-6'}/>
+            </button>
+            <Link to={"/upload"} className={twMerge(
+                ['flex','items-center','justify-center','p-2'],
+                ['rounded-full','hover:bg-text-default/10']
+            )}
+                  title={"동영상 업로드"}
+            >
+                <MdVideoCall className={'w-6 h-6'}/>
+            </Link>
+            <Link to={"/sign-in"} className={twMerge(
+                ['flex','items-center','gap-2','px-4','py-2'],
+                ['border','border-divider','rounded-full'],
+                ['text-secondary-main','font-medium','hover:bg-secondary-main/10']
+            )}>
+                <FaRegUserCircle className={'w-5 h-5'}/>
+                <span className={'text-sm'}>로그인</span>
+            </Link>
+        </div>
+    </header>
 }
 export default Header;
